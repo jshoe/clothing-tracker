9523fc58e8f4cccb1934a9060fe1e7555edbd8a1
portdevil <portdevil@gmail.com>
2014-03-19 22:53:34 -0700

    making progress on basic functions

--- main.py
+++ main.py
@@ -6,32 +6,59 @@ import pprint
 program_version = 0.1
 active_types = []
 # TODO: How to handle the active types and stuff: if the list is empty, prompt the user after they make their selections for the day...
+# TODO: Parse through the JSON file and make a list of all the valid types.
+
+class Item:
+    """An item of clothing."""
+    
+    def __init__(self, data):
+        self.type = data['type']
+        self.message = data['message']
+        self.details = data['details']
+        self.worn = data['worn']
+        self.last_worn = self.worn[-1]
+        self.washed = data['washed']
+        self.last_washed = data['washed'][-1]
+        if 
 
 def launch():
     """Launches the program and loads user data into memory."""
     launch_text()
-    if 
-    global user_data
-    user_data = json.load(open('user_data.txt'))
-    if user_data['user_name'] == null:
-        
-    - Load all the JSON things
+    data = json.load(open('data.txt')) # TODO: make the data.txt file if not there
+    
+    inventory = []
+    for i in range(len(data['items'])):
+        inventory.append(Item(data['items'][i]))
+
+    greet_user()
+
     today = datetime.today() <== datetime object 
     last_access = dictionary['last_access']
     if today > last_access:
         select_clothes()
+    exit()
 
-def enter_user_name():
-    
-        
+def greet_user():
+    """Prompts the user for a name if not stored, and then greets them."""
+    nonlocal data
+    try:
+        name = data['user_name']
+        if (name == None) or (name == ''): # check if this will even generate a KeyError
+            name = input("Please enter your name:\n>>> ")
+    except KeyError, e:
+        name = input("Please enter your name:\n>>> ")
+    print("Hello there, " + name + ".")
+
 def launch_text():
     """Displays the program welcome message."""
     print("Welcome to 'Wardrobe'.")
     print("Version " + program_version + "\n")
-
-def close():
-    """Closes the program and exports the JSON data to file."""
-    - json export with tabs from dictionary
+    
+def exit():
+    """Exports the user data to a text file in JSON and exits."""
+    nonlocal data
+    with open('data.txt', 'w') as outfile:
+        json.dump(data, outfile, indent = 2, sort_keys = True)
 
 def main_display():
     """Shows the main display of clothing display."""
@@ -65,8 +92,4 @@ def select_clothes_menu_text():
     print("[4] Reshuffle top and select a new one.")
     print("[5] Return to Main Menu.\n")
 
-def main():
-    """Controls the execution of the program."""
-    launch()
-    
-main()
+launch()
